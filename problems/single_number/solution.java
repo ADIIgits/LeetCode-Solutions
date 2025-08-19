d06919b03daf1b8b4fe817a68312200a139a2054
ADIIgits <adityalpu27@gmail.com>
2025-08-19 02:50:23 +0000

    Sync LeetCode submission - Single Number (java)

--- problems/single_number/solution.java
+++ problems/single_number/solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int sumor = 0;
-        for(int el:nums){
-            sumor^=el;
+        int el = 0;
+        for(int num:nums){
+            el^=num;
         }
-        return sumor;
+        return el;
     }
 }
