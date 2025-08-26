b78ae5d41114e5f0b4d81fa6e204fe7688c2e5f9
ADIIgits <adityalpu27@gmail.com>
2025-08-26 13:49:18 +0000

    Sync LeetCode submission - Permutations (java)

--- problems/permutations/solution.java
+++ problems/permutations/solution.java
@@ -1,29 +1,23 @@
 class Solution {
-    public static void swap(int[] nums,int a,int b){
-        int temp = nums[a];
-        nums[a]=nums[b];
-        nums[b]=temp;
-    }
-    public static void permutation(int[] nums,List<List<Integer>>ans,int idx){
-        if(idx==nums.length){
-            List<Integer> newnums = new ArrayList<>();
-            for(int k:nums){
-                newnums.add(k);
-            }
-            ans.add(newnums);
+    public static void permutation(int[] nums, List<List<Integer>> ans,List<Integer> list, boolean[] visited){
+        if(list.size()==nums.length){
+            ans.add(new ArrayList<>(list));
             return;
         }
-        for(int i=idx;i<nums.length;i++){
-            
-            swap(nums,idx,i);
-            permutation(nums,ans,idx+1);
-            swap(nums,idx,i);
-
+        for(int i=0;i<nums.length;i++){
+            if(visited[i]) continue;
+            visited[i]=true;
+            list.add(nums[i]);
+            permutation(nums,ans,list,visited);
+            list.remove(list.size()-1);
+            visited[i]=false;
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        permutation(nums,ans,0);
+        List<Integer> list = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        permutation(nums,ans,list,visited);
         return ans;
     }
 }
