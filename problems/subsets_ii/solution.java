4bdc23d0efc8b4d40c7c7268ebaa653dc6afcb78
ADIIgits <adityalpu27@gmail.com>
2025-09-09 16:58:33 +0000

    Sync LeetCode submission - Subsets II (java)

--- problems/subsets_ii/solution.java
+++ problems/subsets_ii/solution.java
@@ -1,22 +1,28 @@
 class Solution {
-    public static void subsets(int[] nums,List<List<Integer>> ans,List<Integer> list,int i){
+    public static void subs(int[] nums,List<List<Integer>> ans,List<Integer> list,boolean[] visited,int i){
+        int n = nums.length;
         if(i==nums.length){
             ans.add(new ArrayList<>(list));
             return;
         }
         list.add(nums[i]);
-        subsets(nums,ans,list,i+1);
+        subs(nums,ans,list,visited,i+1);
         list.remove(list.size()-1);
         int idx = i+1;
-        while(idx<nums.length && nums[idx-1]==nums[idx]) idx++;
-        subsets(nums,ans,list,idx);
-
+        while(idx<n && nums[idx-1]==nums[idx]){
+            idx++;
+        }
+        subs(nums,ans,list,visited,idx);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
+        boolean[] visited = new boolean[n]; 
         Arrays.sort(nums);
-        subsets(nums,ans,list,0);
+        subs(nums,ans,list,visited,0);
         return ans;
+
+        
     }
 }
