d868001a5d21b469e68ded98ea833e8e1d1fb64f
ADIIgits <adityalpu27@gmail.com>
2025-09-09 15:13:52 +0000

    Sync LeetCode submission - Binary Search (java)

--- problems/binary_search/solution.java
+++ problems/binary_search/solution.java
@@ -1,15 +1,15 @@
 class Solution {
-    public static int bsearch(int[] arr,int t,int s,int e){
-        if(s<=e){
-            int mid = s + (e-s)/2; //or (s+e)/2;
-            if(arr[mid]==t) return mid;
-            else if(arr[mid]<t) return bsearch(arr,t,mid+1,e);
-            else return bsearch(arr,t,s,mid-1);
-        }
-        return -1;
+    public static int recsearch(int[] nums,int target,int s,int e){
         
+        if(s<=e){
+            int mid = s+(e-s)/2;
+            if(nums[mid]==target) return mid;
+            else if(target<nums[mid]) return recsearch(nums,target,s,mid-1);
+            else if(target>nums[mid]) return recsearch(nums,target,mid+1,e);
+        }
+        return -1;  
     }
     public int search(int[] nums, int target) {
-        return bsearch(nums,target,0,nums.length-1);
+        return recsearch(nums,target,0,nums.length-1);
     }
 }
