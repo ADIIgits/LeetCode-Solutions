228c321a988410ae09219ab9c0902d0dccc201ca
ADIIgits <adityalpu27@gmail.com>
2025-09-15 18:28:21 +0000

    Sync LeetCode submission - N-Queens (java)

--- problems/n-queens/solution.java
+++ problems/n-queens/solution.java
@@ -1,78 +1,74 @@
 class Solution {
     public static boolean isSafe(char[][] board,int row,int col){
         int n = board.length;
-        //no need to check row, cuz we are already traversing row wise,
+        if(board[row][col]=='Q') return false;
+        //check row
+        for(int i=0;i<n;i++){
+            if(board[row][i]=='Q') return false;
+        }
+        //check col
         for(int i=0;i<n;i++){
             if(board[i][col]=='Q') return false;
         }
-
-        int r = row; int c = col;
-       
-        //bottom-right
-        while(r<n && c<n){
-            if(board[r][c]=='Q') return false;
-            r++;c++;
-        }
-
+        int r,c;
         r=row;c=col;
-        //bottom-left
-        while(r<n && c>=0){
-            if(board[r][c]=='Q') return false;
-            r++;c--;
-        }
-
-        r=row;c=col;
-        //upper-right
-        while(r>=0 && c<n){
-            if(board[r][c]=='Q') return false;
-            r--;c++;
-        }
-
-        r=row;c=col;
-        //upper-left
+        //check top left
         while(r>=0 && c>=0){
             if(board[r][c]=='Q') return false;
             r--;c--;
         }
-
+        r=row;c=col;
+        //check top right
+        while(r>=0 && c<n){
+            if(board[r][c]=='Q') return false;
+            r--;c++;
+        }
+         r=row;c=col;
+        //check bottom left
+        while(r<n && c>=0){
+            if(board[r][c]=='Q') return false;
+            r++;c--;
+        }
+         r=row;c=col;
+        //check bottom right
+        while(r<n && c<n){
+            if(board[r][c]=='Q') return false;
+            r++;c++;
+        }
         return true;
-        
 
     }
-    public static void nqeen(char[][] board,List<List<String>> ans,int row){
+    public static void nqeens(List<List<String>> ans,char[][] board,int row){
         int n = board.length;
         if(row==n){
-            List<String> list = new ArrayList<>();
+            List<String> boardconfig = new ArrayList<>();
             for(int i=0;i<n;i++){
                 String s = "";
                 for(int j=0;j<n;j++){
                     s+=board[i][j];
                 }
-                list.add(s);
+                boardconfig.add(s);
             }
-            ans.add(list);
+            ans.add(boardconfig);
             return;
         }
-
-        for(int i=0;i<n;i++){
-            if(isSafe(board,row,i)){
-                board[row][i] = 'Q';
-                nqeen(board,ans,row+1);
-                board[row][i]= '.';
+        for(int j=0;j<n;j++){
+            if(isSafe(board,row,j)){ //checking
+                board[row][j]='Q'; // and placing
+                nqeens(ans,board,row+1); //repeatedly. untill row = n.
+                board[row][j]='.'; //undo the changes while returning
             }
-
         }
     }
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
-        
         char[][] board = new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 board[i][j]='.';
             }
         }
-        nqeen(board,ans,0);
+        nqeens(ans,board,0);
         return ans;
     }
 }
