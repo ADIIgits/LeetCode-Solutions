4929cc8e203de0d1acae6e1dcae1f31503365716
ADIIgits <adityalpu27@gmail.com>
2025-09-15 18:41:04 +0000

    Sync LeetCode submission - Check Knight Tour Configuration (java)

--- problems/check_knight_tour_configuration/solution.java
+++ problems/check_knight_tour_configuration/solution.java
@@ -1,59 +1,50 @@
 class Solution {
-    public static boolean check(int[][] grid,int row,int col,int curr){
+    public static boolean knightTour(int[][] grid,int row,int col){
         int n = grid.length;
-        if(row<0 || col<0) return false;
-        if(row>=n || col>=n) return false;
-
+        int curr = grid[row][col];
         if(grid[row][col]==n*n-1) return true;
+        int r,c;
+        r=row+2;c=col+1;
+        if(r<n && c<n){
+            if(grid[r][c]==curr+1) return knightTour(grid,r,c);
+        }
+        r=row+2;c=col-1;
+        if(r<n && c>=0){
+            if(grid[r][c]==curr+1) return knightTour(grid,r,c);
+        }
+        r=row-2;c=col+1;
+        if(r>=0 && c<n){
+            if(grid[r][c]==curr+1) return knightTour(grid,r,c);
+        }
+        r=row-2;c=col-1;
+        if(r>=0 && c>=0){
+            if(grid[r][c]==curr+1) return knightTour(grid,r,c);
+        }
 
-        int r=row-2; int c = col-1;
 
-        if(r>=0 && c>=0 && grid[r][c]==curr+1){
-            curr=grid[r][c];
-            return check(grid,r,c,curr);
-        }
-        r=row-2; c=col+1;
-        if(r>=0 && c<n && grid[r][c]==curr+1){
-            curr=grid[r][c];
-            return check(grid,r,c,curr);
-        }
-        r=row+2; c=col-1;
-        if(r<n && c>=0 && grid[r][c]==curr+1){
-            curr=grid[r][c];
-            return check(grid,r,c,curr);
-        }
-        r=row+2; c=col+1;
-        if(r<n && c<n && grid[r][c]==curr+1){
-            curr=grid[r][c];
-            return check(grid,r,c,curr);
-        }
-        r=row-1; c=col-2; 
-        if(r>=0 && c>=0 && grid[r][c]==curr+1){
-            curr=grid[r][c];
-            return check(grid,r,c,curr);
-        }
-        r=row-1;c=col+2;
-        if(r>=0 && c<n && grid[r][c]==curr+1){
-            curr=grid[r][c];
-            return check(grid,r,c,curr);
+
+        r=row+1;c=col+2;
+        if(r<n && c<n){
+            if(grid[r][c]==curr+1) return knightTour(grid,r,c);
         }
         r=row+1;c=col-2;
-        if(r<n && c>=0 && grid[r][c]==curr+1){
-            curr=grid[r][c];
-            return check(grid,r,c,curr);
+        if(r<n && c>=0){
+            if(grid[r][c]==curr+1) return knightTour(grid,r,c);
         }
-        r=row+1;c=col+2;
-        if(r<n && c<n && grid[r][c]==curr+1){
-            curr=grid[r][c];
-            return check(grid,r,c,curr);
+        r=row-1;c=col+2;
+        if(r>=0 && c<n){
+            if(grid[r][c]==curr+1) return knightTour(grid,r,c);
         }
+        r=row-1;c=col-2;
+        if(r>=0 && c>=0){
+            if(grid[r][c]==curr+1) return knightTour(grid,r,c);
+        }
+
         return false;
+
     }
     public boolean checkValidGrid(int[][] grid) {
-        int n = grid.length;
-        if(grid[0][0]!=0)return false;
-        return check(grid,0,0,0);
-       
-
+        if(grid[0][0]!=0) return false;
+        return knightTour(grid,0,0);
     }
 }
