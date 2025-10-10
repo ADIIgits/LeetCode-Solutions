c4334f67d9e7fb672ee1db9b9e3aea07d0c92818
ADIIgits <adityalpu27@gmail.com>
2025-10-10 04:51:02 +0000

    Sync LeetCode submission - Find the Index of the First Occurrence in a String (java)

--- problems/find_the_index_of_the_first_occurrence_in_a_string/solution.java
+++ problems/find_the_index_of_the_first_occurrence_in_a_string/solution.java
@@ -1,28 +1,14 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int he = haystack.length();
-        int ne = needle.length();
-        int base = 256; int mod = 101; int h=1;
-        if(he<ne) return -1;
-        
-        int needleHash = 0; int haystackHash = 0;
-        for(int i=0;i<ne-1;i++){
-            h=(h*base)%mod;
-        }
-        for(int i=0;i<ne;i++){
-            needleHash = (needleHash * base + needle.charAt(i))%mod;
-            haystackHash = (haystackHash * base + haystack.charAt(i))%mod;
-        }
-        
-        for(int i = 0; i<=he-ne;i++){
-            System.out.print(haystackHash+":"+needleHash+"  ");
-            if(haystackHash == needleHash && haystack.substring(i,i+ne).equals(needle)){
-                return i;
+        int n = haystack.length();
+        int m = needle.length();
+        for(int i=0;i<=n-m;i++){
+            int j=0;
+            while(j<m){
+                if(haystack.charAt(i+j)==needle.charAt(j)) j++;
+                else break;
             }
-            if(i<he-ne){
-                haystackHash = (base*(haystackHash - haystack.charAt(i)*h) + haystack.charAt(i+ne))%mod;
-                haystackHash = (haystackHash + mod)%mod;
-            }
+            if(j>=m) return i;
         }
         return -1;
     }
