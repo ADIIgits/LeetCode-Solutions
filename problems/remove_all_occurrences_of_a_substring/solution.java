3658171373571a54703e39d4f7f069f945121bb9
ADIIgits <adityalpu27@gmail.com>
2025-10-14 11:29:18 +0000

    Sync LeetCode submission - Remove All Occurrences of a Substring (java)

--- problems/remove_all_occurrences_of_a_substring/solution.java
+++ problems/remove_all_occurrences_of_a_substring/solution.java
@@ -1,26 +1,44 @@
 class Solution {
-    public static int foundOccurence(String s,String part){
-        int sa = 0; int pa = 0; int se = s.length(); int pe = part.length();
-        for(int i=0;i<=se-pe;i++){
-            if(s.substring(i,i+pe).equals(part))
-            return i;
+    public static int foundOccurence(String haystack, String needle){
+        int base=256; int mod = 121; int h=1;
+        
+        int n=haystack.length(); int m = needle.length();
+        if((n<=0) || (m<=0) || (n<m)) return -1;
+        //calcing base^m-1
+        for(int i=0;i<m-1;i++){
+            h=(h*base)%mod;
+        }
+        int haystackhash=0; int needlehash=0;
+        //calcing first window and needle
+        for(int i=0;i<m;i++){
+            haystackhash = (haystackhash*base + haystack.charAt(i))%mod;
+            needlehash = (needlehash*base + needle.charAt(i))%mod;
+        }
+        //rolling hash
+        for(int i=0;i<=n-m;i++){
+            if(haystackhash == needlehash && haystack.substring(i,i+m).equals(needle)) return i;
+            if(i<n-m){
+                haystackhash = ((haystackhash - haystack.charAt(i)*h )*base + haystack.charAt(i+m))%mod;
+                haystackhash = (haystackhash+mod)%mod;
+            }
         }
         return -1;
+
+    }
+    public static String findAndremoveOccurence(String s,int pos,String part){
+        int n = s.length();
+        int m = part.length();
+        return s.substring(0,pos) + s.substring(pos+m,n);
         
     }
-    public static String removeOcc(String s, String part, int pos){
-        String news = "";
-        news+=s.substring(0,pos);
-        news+=s.substring(pos+part.length(),s.length());
-        System.out.print(news+" ");
-        return news;
-    }
+        
     public String removeOccurrences(String s, String part) {
+        
         int p = foundOccurence(s,part);
         while(s.length()>0 && p!=-1){
-            s = removeOcc(s,part,p);
+            s = findAndremoveOccurence(s,p,part);
             p = foundOccurence(s,part);
-        } 
+        }
         return s;
     }
 }
