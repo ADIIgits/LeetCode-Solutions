2f33fcbb409966c46db9a07d16e7562431c454af
ADIIgits <adityalpu27@gmail.com>
2025-10-14 12:54:51 +0000

    Sync LeetCode submission - Permutation in String (java)

--- problems/permutation_in_string/solution.java
+++ problems/permutation_in_string/solution.java
@@ -1,28 +1,25 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int s1len = s1.length(); int s2len = s2.length();
-        if(s1len>s2len) return false;
-        int[] map1 = new int[26];
-        int[] map2 = new int[26];
-        for(int i=0;i<s1.length();i++){
-            map1[s1.charAt(i)-'a']++;
-            map2[s2.charAt(i)-'a']++;
+        int m = s1.length();
+        int n = s2.length();
+        if(n<m) return false;
+        int[] freq = new int[26];
+        int[] windowfreq = new int[26];
+        for(int i=0;i<m;i++){
+            freq[s1.charAt(i)-'a']++;
+            windowfreq[s2.charAt(i)-'a']++;
         }
-        
-        for(int i=0;i<=s2len-s1len;i++){
+        for(int i=0;i<=n-m;i++){
             int j=0;
             for(j=0;j<26;j++){
-                if(map1[j]!=map2[j]) 
-                break; 
-                else continue;
+                if(freq[j]==windowfreq[j]) continue;
+                else break;
             }
             if(j>=26) return true;
-
-            map2[s2.charAt(i)-'a']--;
-            if(i<s2len-s1len)
-            map2[s2.charAt(i+s1len)-'a']++;
+            windowfreq[s2.charAt(i)-'a']--;
+            if(i<n-m)
+            windowfreq[s2.charAt(i+m)-'a']++;
         }
         return false;
-
-    }
+    } 
 }
