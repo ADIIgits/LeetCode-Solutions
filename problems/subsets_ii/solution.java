412765236f5d743686c38df192b452e48389d73a
ADIIgits <adityalpu27@gmail.com>
2025-10-16 13:10:58 +0000

    Sync LeetCode submission - Subsets II (java)

--- problems/subsets_ii/solution.java
+++ problems/subsets_ii/solution.java
@@ -1,28 +1,23 @@
 class Solution {
-    public static void subs(int[] nums,List<List<Integer>> ans,List<Integer> list,boolean[] visited,int i){
+    public static void generate(int[] nums, List<List<Integer>> allsubsets, List<Integer> subset,int i){
         int n = nums.length;
-        if(i==nums.length){
-            ans.add(new ArrayList<>(list));
-            return;
+        if(i==n){
+            allsubsets.add(new ArrayList<>(subset)); return;
         }
-        list.add(nums[i]);
-        subs(nums,ans,list,visited,i+1);
-        list.remove(list.size()-1);
-        int idx = i+1;
-        while(idx<n && nums[idx-1]==nums[idx]){
-            idx++;
-        }
-        subs(nums,ans,list,visited,idx);
+        subset.add(nums[i]);
+        generate(nums,allsubsets,subset,i+1);
+        subset.remove(subset.size()-1);
+        int j=i;
+        while(j<n && nums[i]==nums[j]) j++;
+        generate(nums,allsubsets,subset,j);
+
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        int n = nums.length;
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        boolean[] visited = new boolean[n]; 
         Arrays.sort(nums);
-        subs(nums,ans,list,visited,0);
-        return ans;
+        List<List<Integer>> allsubsets = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        generate(nums,allsubsets,subset,0);
+        return allsubsets;
 
-        
     }
 }
