fdc33ed27e1285d20e40bda351a242ebeb470dbb
ADIIgits <adityalpu27@gmail.com>
2025-10-16 12:34:25 +0000

    Sync LeetCode submission - Subsets (java)

--- problems/subsets/solution.java
+++ problems/subsets/solution.java
@@ -1,19 +1,20 @@
 class Solution {
-    public static void getsubs(int[] nums,List<List<Integer>> ans,List<Integer> list,int i){
-        if(i==nums.length){
-            ans.add(new ArrayList<>(list));
+    public static void generate(int[] nums,List<List<Integer>> allsubsets, List<Integer> subset,int i){
+        int n=nums.length;
+        if(i==n){
+            allsubsets.add(new ArrayList<>(subset));
             return;
         }
-        list.add(nums[i]);
-        getsubs(nums,ans,list,i+1);
-        list.remove(list.size()-1);
-        getsubs(nums,ans,list,i+1);
-
+        subset.add(nums[i]);
+        generate(nums,allsubsets,subset,i+1);
+        subset.remove(subset.size()-1);
+        generate(nums,allsubsets,subset,i+1);
     }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        getsubs(nums,ans,list,0);
-        return ans;
+        List<List<Integer>> allsubsets = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        generate(nums,allsubsets,subset,0);
+        return allsubsets;
+
     }
 }
