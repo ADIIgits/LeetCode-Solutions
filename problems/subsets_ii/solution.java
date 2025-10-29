abae163227681d2036be78d8ca546ef1e573cd9a
ADIIgits <adityalpu27@gmail.com>
2025-10-29 09:45:50 +0000

    Sync LeetCode submission - Subsets II (java)

--- problems/subsets_ii/solution.java
+++ problems/subsets_ii/solution.java
@@ -1,23 +1,24 @@
 class Solution {
-    public static void generate(int[] nums, List<List<Integer>> allsubsets, List<Integer> subset,int i){
+    
+    public static void generate(int[] nums,List<List<Integer>> AllSubsets,List<Integer> subset,int i){
         int n = nums.length;
         if(i==n){
-            allsubsets.add(new ArrayList<>(subset)); return;
+            AllSubsets.add(new ArrayList<>(subset));
+            return;
         }
         subset.add(nums[i]);
-        generate(nums,allsubsets,subset,i+1);
+        generate(nums,AllSubsets,subset,i+1);
         subset.remove(subset.size()-1);
-        int j=i;
-        while(j<n && nums[i]==nums[j]) j++;
-        generate(nums,allsubsets,subset,j);
-
+        int idx = i+1;
+        while(idx<n && nums[idx-1]==nums[idx]) idx++;
+        generate(nums,AllSubsets,subset,idx);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> allsubsets = new ArrayList<>();
+        List<List<Integer>> AllSubsets = new ArrayList<>();
         List<Integer> subset = new ArrayList<>();
-        generate(nums,allsubsets,subset,0);
-        return allsubsets;
+        generate(nums,AllSubsets,subset,0);
+        return AllSubsets;
 
     }
 }
