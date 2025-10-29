01f0b80a758ca998e45707e05acea9ecafd80c88
ADIIgits <adityalpu27@gmail.com>
2025-10-29 09:21:53 +0000

    Sync LeetCode submission - Subsets (java)

--- problems/subsets/solution.java
+++ problems/subsets/solution.java
@@ -1,20 +1,19 @@
 class Solution {
-    public static void generate(int[] nums,List<List<Integer>> allsubsets, List<Integer> subset,int i){
-        int n=nums.length;
+    public static void generate(int[] nums,List<List<Integer>> AllSubsets,List<Integer> subset,int i){
+        int n = nums.length;
         if(i==n){
-            allsubsets.add(new ArrayList<>(subset));
+            AllSubsets.add(new ArrayList<>(subset));
             return;
         }
         subset.add(nums[i]);
-        generate(nums,allsubsets,subset,i+1);
+        generate(nums,AllSubsets,subset,i+1);
         subset.remove(subset.size()-1);
-        generate(nums,allsubsets,subset,i+1);
+        generate(nums,AllSubsets,subset,i+1);
     }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> allsubsets = new ArrayList<>();
+         List<List<Integer>> AllSubsets = new ArrayList<>();
         List<Integer> subset = new ArrayList<>();
-        generate(nums,allsubsets,subset,0);
-        return allsubsets;
-
+        generate(nums,AllSubsets,subset,0);
+        return AllSubsets;
     }
 }
