9a15dba2fc02853dc3414e7f7b0a261dfa74268d
ADIIgits <adityalpu27@gmail.com>
2025-11-01 15:54:52 +0000

    Sync LeetCode submission - Maximum Subarray (cpp)

---
+++ problems/maximum_subarray/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxsum = INT_MIN; int sum=0;
+        for(int el:nums){
+            sum+=el;
+            maxsum = max(sum,maxsum);
+            if(sum<0)sum=0;
+        }
+        return maxsum;
+    }
+};
