d5f370769b9445f6ab62adccca6a5b9661c3ff40
ADIIgits <adityalpu27@gmail.com>
2025-11-05 04:55:27 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- problems/two_sum/solution.cpp
+++ problems/two_sum/solution.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
-    bool isin(vector<int> &arr,int t)
-    {
-        for(int i=0;i<arr.size();i++)
-        {
-            if(arr[i]==t)
-            return true;
-        }
-        return false;
-    }
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int> arr;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                    if(i!=j && (nums[j]+nums[i]==target))
-                    {
-                        
-                        arr.push_back(i);arr.push_back(j);
-                        return arr;
-                       
-                    }
-                    
+        int n = nums.size();
+        unordered_map<int,int> cache;
+        for(int i=0;i<n;i++){
+            int compliment = target-nums[i];
+            auto hit = cache.find(compliment);
+            if(hit!=cache.end()){
+                return {hit->second,i};
             }
+            else cache[nums[i]]=i;
         }
-
+        return {-1,-1};
         
-        arr.push_back(-1);
-        arr.push_back(-1);
-        return arr;
     }
 };
