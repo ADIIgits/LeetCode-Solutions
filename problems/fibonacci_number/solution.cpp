91da7909f97244082aca4ae62b30d1de11c928fd
ADIIgits <adityalpu27@gmail.com>
2026-01-01 16:41:00 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 || n==1 ) return n;
-        int curr=1; int prev=0; int sum=0;
-        for(int i=1;i<n;i++){
-            sum = curr+prev;
-            prev=curr;
-            curr=sum;
-        }
-        return sum;
+        if(n==0 || n==1) return n;
+        return fib(n-1)+fib(n-2);
     }
 };
