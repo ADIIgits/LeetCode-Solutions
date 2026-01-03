2fa544e81cbaf44f9cb999fc7d9a5817868b0bcb
ADIIgits <adityalpu27@gmail.com>
2026-01-03 16:58:50 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -2,6 +2,12 @@ class Solution {
 public:
     int fib(int n) {
         if(n==0 || n==1) return n;
-        return fib(n-1)+fib(n-2);
+        vector<int> fibarr(n+1,-1);
+        fibarr[0]=0;
+        fibarr[1]=1;
+        for(int i=2;i<=n;i++){
+            fibarr[i] = fibarr[i-1]+fibarr[i-2];
+        }
+        return fibarr[n];
     }
 };
