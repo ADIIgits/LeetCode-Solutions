c1ac9e8ce53b5a4ebe407df9ded11637951b0f7d
ADIIgits <adityalpu27@gmail.com>
2026-01-22 04:27:12 +0000

    Sync LeetCode submission - Single Number (cpp)

--- problems/single_number/solution.cpp
+++ problems/single_number/solution.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int el:nums){
-            ans^=el;
-        }
-        return ans;
+        int element=0;
+        for(int el:nums) element^=el;
+        return element;
     }
 };
