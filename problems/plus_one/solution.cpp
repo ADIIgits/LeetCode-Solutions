88770e01cea140489165ce2cd6c765d5a7667dc0
ADIIgits <adityalpu27@gmail.com>
2026-01-28 13:10:06 +0000

    Sync LeetCode submission - Plus One (cpp)

--- problems/plus_one/solution.cpp
+++ problems/plus_one/solution.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& dig) {
-        int n=dig.size();
-        int k=n-1;
-        
-        while(k>=0)
-        {
-            if(dig[k]<9)
-            {
-                dig[k]++;
-                break;
-            }
-            else
-            {
-                dig[k]=0;
-                k--;
-            }
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> ans;
+        int n = digits.size();
+        int q=n-1; int carry=1;
+        while(q>=0){
+            int sum = digits[q] + carry;
+            if(sum>9){
+                carry = sum/10;
+                sum = sum%10;
+            } else carry=0;
+            ans.push_back(sum);
+            q--;
         }
-        if(k<0)
-        dig.insert(dig.begin(),1);
-
-        return dig;
-        
+        if(carry!=0) ans.push_back(carry);
+        for(int el:ans){
+            cout<<el<<" ";
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
