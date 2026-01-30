3fd8d1f10fecb16a541131d557e951fb2e8a9ad8
ADIIgits <adityalpu27@gmail.com>
2026-01-30 19:25:16 +0000

    Sync LeetCode submission - Valid Palindrome (cpp)

--- problems/valid_palindrome/solution.cpp
+++ problems/valid_palindrome/solution.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int n = s.length();
-        int st=0; int ed=n-1;
-        while(st<ed){
-            if(isalnum(s[st])){
-                if(isalnum(s[ed])){
-                    if(tolower(s[st])!=tolower(s[ed])) return false;
-                    else{st++;ed--;}
-                }
-                else ed--;
-            }
-            else st++;
+        // if(n==2) return s[0]==s[1];
+        int p=0; int q=n-1;
+        while(p<q){
+            if(isalnum(s[p])){
+                if(isalnum(s[q])){
+                    if(toupper(s[p])!=toupper(s[q])) return false;
+                    else {p++;q--;}
+                } else q--;  
+            } else p++;
+
         }
         return true;
     }
