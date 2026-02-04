6c72ef43b88783e7e213d2f0c743870c20fe3ce6
ADIIgits <adityalpu27@gmail.com>
2026-02-04 19:48:31 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (cpp)

--- problems/search_in_rotated_sorted_array/solution.cpp
+++ problems/search_in_rotated_sorted_array/solution.cpp
@@ -6,12 +6,12 @@ public:
         while(st<=ed){
             int mid = st + (ed-st)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[st]<=nums[mid]){
-                if(nums[st]<=target && target<nums[mid]) ed=mid-1;
+            if(nums[st]<=nums[mid]){
+                if(nums[st]<=target && target<=nums[mid]) ed=mid-1;
                 else st=mid+1;
             } else {
-                if(nums[mid]<target && target<=nums[ed]) st = mid+1;
-                else ed = mid-1;
+                if(nums[mid]<=target && target<=nums[ed]) st=mid+1;
+                else ed=mid-1;
             }
         }
         return -1;
