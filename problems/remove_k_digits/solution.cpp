03d021795170566b9f756f18193cb104c273f3a1
ADIIgits <adityalpu27@gmail.com>
2026-02-16 19:23:36 +0000

    Sync LeetCode submission - Remove K Digits (cpp)

--- problems/remove_k_digits/solution.cpp
+++ problems/remove_k_digits/solution.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    string removeKdigits(string num, int k) {
-        string s="";
-        for(char c:num){
-            // hunting for smalest c untill k chances exhaust.
-            while(!s.empty() && s.back()>c && k>0){
-                s.pop_back();
+    string removeKdigits(string nums, int k) {
+        
+
+        string st = "";
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            char curr = nums[i];
+            while(!st.empty() && st.back()>nums[i] && k>0){
+                st.pop_back();
                 k--;
             }
-            s.push_back(c);
+            st.push_back(nums[i]);
         }
-        //imagine a string (or a part of string) where all digits are in ascending order. no pop will be triggered. thus k wont ever be 0. so we simply remove rest of the k digits from the back (cuz thats where largest digits are).
-        while(k>0){
-            s.pop_back(); k--;
+        int i=0;
+        while(!st.empty() && k>0){
+            st.pop_back(); k--;
         }
-        int j=0;
-        while(j<s.length() && s[j]=='0') j++;
-        s=s.substr(j);
-        if(s=="") return "0";
-        else return s;
+        while(st[i]=='0')i++;
+        st = st.substr(i);
+        if(st=="") return "0";
+        else return st;
 
-        
     }
 };
