6a75bbf506181f98682ab6e8a5fd849778433c9f
ADIIgits <adityalpu27@gmail.com>
2026-02-19 18:16:00 +0000

    Sync LeetCode submission - Minimum Window Substring (cpp)

--- problems/minimum_window_substring/solution.cpp
+++ problems/minimum_window_substring/solution.cpp
@@ -1,35 +1,34 @@
 class Solution {
-    bool valid(vector<int> mapwindow,vector<int> mapt){
+    bool isvalid(vector<int> alphas,vector<int> alphat){
         for(int i=0;i<58;i++){
-            if(mapwindow[i]<mapt[i]) return false;
+            if(alphas[i]<alphat[i]) return false;
         }
         return true;
     }
 public:
     string minWindow(string s, string t) {
-        int minlen = INT_MAX; int start = 0;
-        //window slide krvadunga, map me freq store krvata rahunga
-        //ek dusrre map me target ki freq pehle store krvalunga. at everypoint dono ko compare krta rahunga, if they match save the length. and return max among them.
-        if(s.length() < t.length()) return "";
         int n = s.length();
-        int p = 0; int q=0;
-        vector<int> mapwindow(58,0);
-        vector<int> mapt(58,0);
-        for(char c:t) mapt[c-'A']++;
+        int m = t.length();
+        vector<int> alphat(58,0);
+        vector<int> alphas(58,0);
+        for(int i=0;i<m;i++) alphat[t[i]-'A']++;
+        int minsize = INT_MAX;
+        int start = 0;
+        int p=0; int q=0;
         while(q<n){
-            // char c = s[q];
-            mapwindow[s[q]-'A']++;
-            while(p<n && valid(mapwindow,mapt)){
-                if(q-p+1 < minlen){
-                    minlen = q-p+1;
-                    start = p;
-                } 
-                mapwindow[s[p]-'A']--;
+            alphas[s[q]-'A']++;
+            while(p<n && isvalid(alphas,alphat)){
+                alphas[s[p]-'A']--;
+                if(minsize>q-p+1){
+                    minsize = q-p+1;
+                    start=p;
+                }
                 p++;
             }
             q++;
         }
-        if(minlen==INT_MAX) return "";
-        return s.substr(start,minlen);
+        if(minsize==INT_MAX) return "";
+        return s.substr(start,minsize);
+        
     }
 };
