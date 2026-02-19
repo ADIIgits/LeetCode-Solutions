816e9e41b46f3091e319699f024b67491f8f18c8
ADIIgits <adityalpu27@gmail.com>
2026-02-19 19:14:05 +0000

    Sync LeetCode submission - Maximum Points You Can Obtain from Cards (cpp)

--- problems/maximum_points_you_can_obtain_from_cards/solution.cpp
+++ problems/maximum_points_you_can_obtain_from_cards/solution.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int maxScore(vector<int>& nums, int k) {
-       // since we can only choose the points from either end and that too, however we want. we wanna maximize our points. so we reverse logic. we find the subarray of length n-k that has minimum points-sum. cuz that would mean the rest of the array has maximum points-sum;
-        int totalpoints = 0;
-        for(int pts:nums) totalpoints+=pts;
-       int pointsum = 0;
-       int minpts = INT_MAX;
-       int n = nums.size();
-
-       int size = n-k;
-       int p=0; int q=0;
-       while(q<n){
-            pointsum+=nums[q];
-            //currlen = q-p;
-            while((q-p+1) > size){
-                pointsum-=nums[p];
+    int maxScore(vector<int>& cp, int k) {
+        int n = cp.size();
+        //goal is to find minsum subarray of size n-k.
+        int totalsum = 0;
+        for(int i=0;i<n;i++) totalsum+=cp[i];
+        int sum=0; int minsum = totalsum;
+        int p=0;int q=0;
+        while(q<n){
+            sum+=cp[q];
+            while(q-p+1>n-k){
+                sum-=cp[p];
                 p++;
             }
-            if((q-p+1)==size) minpts = min(minpts,pointsum);
+            if(q-p+1==n-k) minsum = min(minsum,sum);
             q++;
-       }
-       return totalpoints - minpts;
+        }
+        return totalsum-minsum;
     }
 };
