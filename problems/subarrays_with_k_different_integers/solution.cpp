42fd9a35cf50fd53f6254eff5cb082b31438ceee
ADIIgits <adityalpu27@gmail.com>
2026-02-19 18:47:28 +0000

    Sync LeetCode submission - Subarrays with K Different Integers (cpp)

--- problems/subarrays_with_k_different_integers/solution.cpp
+++ problems/subarrays_with_k_different_integers/solution.cpp
@@ -1,24 +1,25 @@
 class Solution {
-    int calcsub(vector<int>& nums, int k){
-        unordered_map<int,int> map;
-        // unordered_set<int> distinct; you dont need it, cuz map.size() already gives you that. distinct. remeber.
+    int goodsub(vector<int>& nums, int k){
         int n = nums.size();
-        int p=0; int q=0;
         int count=0;
+        int p=0; int q=0;
+        unordered_map<int,int> freq;
         while(q<n){
-            map[nums[q]]++;
-            while(map.size()>k){
-                map[nums[p]]--;
-                if(map[nums[p]]<=0) map.erase(nums[p]);
+            freq[nums[q]]++;
+            
+            while(p<q && freq.size()>k){
+                freq[nums[p]]--;
+                if(freq[nums[p]]==0) freq.erase(nums[p]);
                 p++;
             }
-            if(map.size()<=k) count+=(q-p+1);
+            if(freq.size()<=k) count += q-p+1;
             q++;
         }
+        cout<<count<<" ";
         return count;
     }
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return calcsub(nums,k) - calcsub(nums,k-1);
+        return goodsub(nums,k) - goodsub(nums,k-1);
     }
 };
