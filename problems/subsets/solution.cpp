3b9e02602e61c147a20256f4c3b949a2edb39414
ADIIgits <adityalpu27@gmail.com>
2026-02-24 10:23:08 +0000

    Sync LeetCode submission - Subsets (cpp)

--- problems/subsets/solution.cpp
+++ problems/subsets/solution.cpp
@@ -1,20 +1,20 @@
 class Solution {
-    void subs(vector<int> &nums,vector<int> &vec,vector<vector<int>> &ans,int i){
+    void sub(vector<int>& nums,vector<int> &vec,vector<vector<int>> &ans,int i){
         int n = nums.size();
         if(i==n){
-            ans.push_back(vector<int> (vec));
+            ans.push_back(vec);
             return;
-        }
+        } 
         vec.push_back(nums[i]);
-        subs(nums,vec,ans,i+1);
+        sub(nums,vec,ans,i+1);
         vec.pop_back();
-        subs(nums,vec,ans,i+1);
+        sub(nums,vec,ans,i+1);
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> vec;
-        subs(nums,vec,ans,0);
+        sub(nums,vec,ans,0);
         return ans;
     }
 };
