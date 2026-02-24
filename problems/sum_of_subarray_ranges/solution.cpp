94c7600ab93824a51e089683a948592c7e3fb0fd
ADIIgits <adityalpu27@gmail.com>
2026-02-24 07:01:54 +0000

    Sync LeetCode submission - Sum of Subarray Ranges (cpp)

--- problems/sum_of_subarray_ranges/solution.cpp
+++ problems/sum_of_subarray_ranges/solution.cpp
@@ -1,17 +1,59 @@
 class Solution {
+    int mod = 1e9 + 7;
+    long long sumsmallest(vector<int>& nums){
+        int n = nums.size();
+        long long sum = 0;
+        stack<int> st;
+        vector<int> nextsmaller(n);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]>=nums[i]){
+                st.pop();
+            }
+            nextsmaller[i] = st.empty()? n : st.top();
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]>nums[i]){
+                st.pop();
+            }
+            int prevsmall = st.empty()? -1 : st.top();
+            int nextsmall = nextsmaller[i];
+            long long subcount = (i-prevsmall)*(nextsmall-i);
+            sum+=(subcount*nums[i]);
+            st.push(i);
+        }
+        return sum;
+    }
+    long long sumlargest(vector<int>& nums){
+        int n = nums.size();
+        long long sum = 0;
+        stack<int> st;
+        vector<int> nextgreater(n);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<=nums[i]){
+                st.pop();
+            }
+            nextgreater[i] = st.empty()? n : st.top();
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]<nums[i]){
+                st.pop();
+            }
+            int prevlarge = st.empty()? -1 : st.top();
+            int nextlarge = nextgreater[i];
+            long long subcount = (i-prevlarge)*(nextlarge-i);
+            sum+=(subcount*nums[i]);
+            st.push(i);
+        }
+        return sum;
+    }
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
         long long sum = 0;
-        
-        for(int i=0;i<n;i++){
-            int mini = INT_MAX; int maxi = INT_MIN;
-            for(int j=i;j<n;j++){
-                mini = min(mini,nums[j]);
-                maxi = max(maxi,nums[j]);
-                sum += maxi - mini;
-            }
-        }
-        return sum;
+        return sumlargest(nums) - sumsmallest(nums);
     }
 };
