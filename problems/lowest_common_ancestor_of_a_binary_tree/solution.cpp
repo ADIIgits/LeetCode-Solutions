219f232685e2674028a9922396c55ed0af7aa878
ADIIgits <adityalpu27@gmail.com>
2026-03-02 10:27:57 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree (cpp)

--- problems/lowest_common_ancestor_of_a_binary_tree/solution.cpp
+++ problems/lowest_common_ancestor_of_a_binary_tree/solution.cpp
@@ -8,20 +8,18 @@
  * };
  */
 class Solution {
-    TreeNode* found(TreeNode* root, TreeNode* p, TreeNode* q){
+    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root==NULL) return NULL;
         if(root==p || root==q) return root;
-        
-        TreeNode* leftf = found(root->left,p,q);
-        TreeNode* rightf = found(root->right,p,q);
-
-        if(leftf && rightf) return root;
-        else if(leftf) return leftf;
-        else if(rightf) return rightf;
-        else return NULL;
+        TreeNode* left = lca(root->left,p,q);
+        TreeNode* right = lca(root->right,p,q);
+        if(left && right) return root;
+        if(left) return left;
+        if(right) return right;
+        return NULL;
     }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return found(root,p,q);
+        return lca(root,p,q);
     }
 };
