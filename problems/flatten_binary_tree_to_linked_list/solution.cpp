4e88522c86a7c34f654bad34b0ba4d70a7d39f80
ADIIgits <adityalpu27@gmail.com>
2026-03-08 10:30:46 +0000

    Sync LeetCode submission - Flatten Binary Tree to Linked List (cpp)

--- problems/flatten_binary_tree_to_linked_list/solution.cpp
+++ problems/flatten_binary_tree_to_linked_list/solution.cpp
@@ -6,26 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-    void preorder(TreeNode* root,vector<TreeNode*> &nodes){
+    void flat(TreeNode* root,vector<TreeNode*> &nodes){
         if(root==NULL) return;
         nodes.push_back(root);
-        if(root->left) preorder(root->left,nodes);
-        if(root->right) preorder(root->right,nodes);
+        if(root->left) flat(root->left,nodes);
+        if(root->right) flat(root->right,nodes);
+
     }
-    
 public:
     void flatten(TreeNode* root) {
         vector<TreeNode*> nodes;
-        preorder(root,nodes);
+        flat(root,nodes);
         int n = nodes.size();
         for(int i=0;i<n-1;i++){
-            TreeNode* rot = nodes[i];
-            rot->left=NULL;
-            rot->right = nodes[i+1];
+            nodes[i]->left=NULL;
+            nodes[i]->right = nodes[i+1];
         }
     }
 };
