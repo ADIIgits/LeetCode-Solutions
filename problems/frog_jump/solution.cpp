4ad94eabe492e166595d2d6f357bf6db0a920942
ADIIgits <adityalpu27@gmail.com>
2026-03-11 10:31:23 +0000

    Sync LeetCode submission - Frog Jump (cpp)

--- problems/frog_jump/solution.cpp
+++ problems/frog_jump/solution.cpp
@@ -1,40 +1,16 @@
 class Solution {
-    int findnextstone(vector<int>& stones,int i,int el){
-        int n = stones.size();
-        int st=i+1; int ed = n-1;
-        while(st<=ed){
-            int mid = st + (ed-st)/2;
-            if(stones[mid]==el) return mid;
-            else if(stones[mid]<el) st = mid+1;
-            else ed=mid-1; 
-        }
-        return -1;
-    }
-    bool cross(vector<int>& stones,int i,int k,vector<vector<int>> &dp){
-        int n = stones.size();
-        if(i==n-1) return true;
-        if(i==-1) return false;
-        if(k<0) return false;
-        if(dp[i][k]!=-1) return dp[i][k];
-        int kminusone  = findnextstone(stones,i,stones[i]+k-1);
-        int korg  = findnextstone(stones,i,stones[i]+k);
-        int kplusone  = findnextstone(stones,i,stones[i]+k+1);
-
-        bool minus1 = cross(stones,kminusone,k-1,dp);
-        bool none = cross(stones,korg,k,dp);
-        bool plus1 = cross(stones,kplusone,k+1,dp);
-
-        if(minus1 || none || plus1) dp[i][k]=1;
-        else dp[i][k]=0;
-
-        return dp[i][k];
-    }
 public:
     bool canCross(vector<int>& stones) {
-        //base case when it reaches last stone, return true
-        // k = stones[i-1]
-        int n = stones.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return cross(stones,0,0,dp);
+        unordered_map<int,unordered_set<int>> dp;
+        for(int s:stones) dp[s]={};
+        dp[0].insert(0);
+        for(int s:stones){
+            for(int k:dp[s]){
+                for(int jump=k-1;jump<=k+1;jump++){
+                    if(jump>0 && dp.count(s+jump)) dp[s+jump].insert(jump);
+                }
+            }
+        }
+        return !dp[stones.back()].empty();
     }
 };
