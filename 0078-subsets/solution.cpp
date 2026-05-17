22f94b473d8eab99ed206b5dc7d34b6cdbde76cb
ADIIgits <adityalpu27@gmail.com>
2026-05-17 17:17:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (57.97%)

--- 0078-subsets/solution.cpp
+++ 0078-subsets/solution.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void generate(vector<int>& nums,int i,vector<int> vec,vector<vector<int>> &ans){
+    void generate(vector<int>& nums,vector<vector<int>> &ans,vector<int> &vec,int i){
         int n = nums.size();
         if(i==n){
             ans.push_back(vec);
             return;
         }
         vec.push_back(nums[i]);
-        generate(nums,i+1,vec,ans);
+        generate(nums,ans,vec,i+1);
         vec.pop_back();
-        generate(nums,i+1,vec,ans);
+        generate(nums,ans,vec,i+1);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> vec;
-        generate(nums,0,vec,ans);
+        generate(nums,ans,vec,0);
         return ans;
     }
 };
