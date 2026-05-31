19a78449a488f515be8e9cb68d573d5435d4ac59
ADIIgits <adityalpu27@gmail.com>
2026-05-31 17:06:20 +0000

    Sync LeetCode submission Runtime - 3 ms (28.07%), Memory - 17.7 MB (8.20%)

--- 1036-rotting-oranges/solution.cpp
+++ 1036-rotting-oranges/solution.cpp
@@ -1,64 +1,53 @@
-class Triplet{
-    public:
-    int row;
-    int col;
-    int time;
-    Triplet(int i,int j, int t){
-        row = i;
-        col = j;
-        time = t;
-    }
-};
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        
         int m = grid.size(); int n = grid[0].size();
-        queue<Triplet> que;
+        int maxminute = 0;
         int fresh = 0;
+        queue<vector<int>> que;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                //push all rotten oranges into queue. all of them are ground zero. time zero.
+                if(grid[i][j]==1) fresh++;
                 if(grid[i][j]==2) que.push({i,j,0});
-                if(grid[i][j]==1) fresh++; // count all fresh oranges
-                //cuz they are about to be infected and we need to verify
             }
         }
-        int maxtime = 0;
         while(!que.empty()){
-            Triplet t = que.front();
-            int row = t.row; int col = t.col; int time = t.time;
+            vector<int> currnode = que.front();
             que.pop();
-            maxtime = max(maxtime,time); //keeps track of time since infection started.
-
-            //check where can we go.
-            bool top = row==0? false : grid[row-1][col]==1;
-            bool right = col==n-1? false: grid[row][col+1]==1;
-            bool left = col==0? false: grid[row][col-1]==1;
-            bool down = row==m-1? false : grid[row+1][col]==1;
-
-            //if we can go - 
-            if(top){
-                grid[row-1][col]=2; //infect them niggas
-                fresh--; // decrease count, cuz one nigga down.
-                // this nigga gonna infect the rest, so we gonna push him into infected oranges queue.
-                que.push({row-1,col,time+1}); 
+            int row = currnode[0];
+            int col = currnode[1];
+            int level = currnode[2];
+            //down
+            int r = row+1; int c = col;
+            if(r < m && grid[r][c]==1){
+                que.push({r,c,level+1});
+                grid[r][c] = 2;
+                fresh--;
             }
-            if(down){
-                grid[row+1][col]=2; fresh--;
-                que.push({row+1,col,time+1});
+            //up
+            r = row-1; c = col;
+            if(r >= 0 && grid[r][c]==1){
+                que.push({r,c,level+1});
+                grid[r][c] = 2;
+                fresh--;
             }
-            if(left){
-                grid[row][col-1]=2; fresh--;
-                que.push({row,col-1,time+1});
+            //left
+            r = row; c = col-1;
+            if(c >= 0 && grid[r][c]==1){
+                que.push({r,c,level+1});
+                grid[r][c] = 2;
+                fresh--;
             }
-            if(right){
-                grid[row][col+1]=2; fresh--;
-                que.push({row,col+1,time+1});
+            //right
+            r = row; c = col+1;
+            if(c < n && grid[r][c]==1){
+                que.push({r,c,level+1});
+                grid[r][c] = 2;
+                fresh--;
             }
+            maxminute = max(maxminute,level);
         }
-        //checking if any good oranges left, or all infected.
-        if(fresh>0) return -1; 
-        return maxtime;
+        if(fresh > 0) return -1;
+        return maxminute;
     }
 };
