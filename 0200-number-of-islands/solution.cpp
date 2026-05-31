d36b61c86592fc321aeb3840371df4bf8cfea2af
ADIIgits <adityalpu27@gmail.com>
2026-05-31 16:31:28 +0000

    Sync LeetCode submission Runtime - 30 ms (33.70%), Memory - 23.6 MB (23.64%)

--- 0200-number-of-islands/solution.cpp
+++ 0200-number-of-islands/solution.cpp
@@ -1,73 +1,51 @@
-class DSU{
-    public:
-    vector<int> parent;
-    vector<int> size;
-    DSU(int n){
-        parent.resize(n);
-        size.resize(n);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            size[i]=1;
-        }
-    }
-    int find(int v){
-        if(parent[v]==v) return v;
-        return parent[v] = find(parent[v]);
-    }
-    void unite(int a, int b){
-        a = find(a);
-        b = find(b);
-
-        if(a!=b){
-            if(size[a] < size[b])
-                swap(a,b);
-            parent[b] = a;
-            size[a] += size[b];
-        }
-    }
-};
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        //loop over all nodes, for every node, that is and island, we check its 4 directions, if they have another island, we merge em using dsu. then at the end we loop over all nodes and count independent connected components.(find(i)==i)
-        int m = grid.size(); //no of rows;
-        int n = grid[0].size(); //no of cols;
-        
-        int N = n*m;
-        DSU dsu(N);
-        auto node = [](int i, int j,int n){
-            return i*n + j; //this will get us the node number
-        };
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='0') continue;
-                //right
-                if(j<n-1 && grid[i][j+1]=='1'){
-                    dsu.unite(node(i,j,n),node(i,j+1,n));
-                } 
-                //left
-                if(j>0 && grid[i][j-1]=='1'){
-                    dsu.unite(node(i,j,n),node(i,j-1,n));
-                } 
-                //top
-                if(i>0 && grid[i-1][j]=='1'){
-                    dsu.unite(node(i,j,n),node(i-1,j,n));
-                } 
-                //bottom
-                if(i<m-1 && grid[i+1][j]=='1'){
-                    dsu.unite(node(i,j,n),node(i+1,j,n));
-                }
-
+    void BFS(vector<vector<char>>& grid,vector<vector<bool>> &vis,int source_row,int source_col){
+        int n = grid.size(); int m = grid[0].size();
+        queue<pair<int,int>> que;
+        que.push({source_row,source_col});
+        vis[source_row][source_col] = true;
+        while(!que.empty()){
+            auto [row,col] = que.front();
+            que.pop();
+            //down
+            int r = row+1; int c = col;
+            if(r<n && grid[r][c]=='1' && !vis[r][c]){
+                vis[r][c] = true;
+                que.push({r,c});
+            }
+            //up
+            r = row-1; c = col;
+            if(r >=0 && grid[r][c]=='1' && !vis[r][c]){
+                vis[r][c] = true;
+                que.push({r,c});
+            }
+            //left
+            r = row; c = col-1;
+            if(c >= 0 && grid[r][c]=='1' && !vis[r][c]){
+                vis[r][c] = true;
+                que.push({r,c});
+            }
+            //right
+            r = row; c = col+1;
+            if(c<m && grid[r][c]=='1' && !vis[r][c]){
+                vis[r][c] = true;
+                que.push({r,c});
             }
         }
-        int count=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='0') continue;
-                if(dsu.find(node(i,j,n))==node(i,j,n)) count++;
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        int n = grid.size(); int m = grid[0].size();
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    count++;
+                    BFS(grid,vis,i,j);
+                }
             }
         }
         return count;
-
     }
 };
