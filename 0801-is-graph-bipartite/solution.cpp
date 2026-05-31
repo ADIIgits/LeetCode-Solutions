98ade9a0f587b58c959dc9940935ecd0cd54bde9
ADIIgits <adityalpu27@gmail.com>
2026-05-31 20:50:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.5 MB (41.73%)

--- 0801-is-graph-bipartite/solution.cpp
+++ 0801-is-graph-bipartite/solution.cpp
@@ -1,43 +1,30 @@
-class DSU{
-    public:
-    vector<int> parent;
-    vector<int> size;
-    DSU(int n){
-        parent.resize(n);
-        size.resize(n);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            size[i]=1;
-        }
-    }
-    int find(int v){
-        if(parent[v]==v) return v;
-        return parent[v]=find(parent[v]);
-    }
-    void unite(int a,int b){
-        a = find(a);
-        b = find(b);
-        if(a!=b){
-            if(size[a] < size[b])
-                swap(a,b);
-            parent[b] = a;
-            size[a] += size[b];
-        }
-    }
-};
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        //if two nodes belong to sae color and have edge between em, not bipartite.
-        int V = graph.size();
-        DSU dsu(V);
-        for(int i=0;i<V;i++){
-            int currnode = i;
-            for(int neighbour:graph[currnode]){
-                if(dsu.find(currnode)==dsu.find(neighbour)) return false;
-                dsu.unite(graph[currnode][0],neighbour);
+    bool can_color(vector<vector<int>>& graph,vector<int> &colored,int src){
+        queue<pair<int,int>> que;
+        que.push({src,0});
+        colored[src] = 0;
+        while(!que.empty()){
+            auto [currnode,currcolor] = que.front();
+            que.pop();
+            for(int node:graph[currnode]){
+                if(currcolor == colored[node]) return false;
+
+                if(colored[node]==-1){
+                    colored[node] = 1-currcolor;
+                    que.push({node,1-currcolor});
+                }
             }
-            
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> vis(n,-1);
+        for(int i=0;i<n;i++){
+            if(vis[i]==-1){
+                if(!can_color(graph,vis,i)) return false;
+            }
         }
         return true;
     }
